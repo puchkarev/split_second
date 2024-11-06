cf879140a00d751c250db1f1f67f04fccda7572e
Victor Puchkarev <ittechnician@gmail.com>
2024-11-06 10:47:22 -0800

    Fixing the package name from example to puchkarev

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,19 +4,19 @@
 GameState* state_ = nullptr;
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_split_1second_MainActivity_initGame(JNIEnv* /*env*/, jclass /*this*/) {
+Java_com_puchkarev_split_1second_MainActivity_initGame(JNIEnv* /*env*/, jclass /*this*/) {
     state_ = new GameState();
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_split_1second_MainActivity_render(JNIEnv* /*env*/, jclass /*this*/) {
+Java_com_puchkarev_split_1second_MainActivity_render(JNIEnv* /*env*/, jclass /*this*/) {
     if (state_ != nullptr) {
         state_->render();
     }
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_split_1second_MainActivity_touchEvent(JNIEnv* /*env*/, jclass /*this*/, int width, int height, float eventX, float eventY) {
+Java_com_puchkarev_split_1second_MainActivity_touchEvent(JNIEnv* /*env*/, jclass /*this*/, int width, int height, float eventX, float eventY) {
     // Event coordinates are in pixels (just like width and length), with bottom left
     // bottom left: at x = 0, y = height
     // top right: x = width, y = 0
